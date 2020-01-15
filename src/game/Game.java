3671b14511d52407f39dd9475b380d7df7195ac6
Eline Verbon <eline.verbon@nedap.com>
2020-01-15 16:56:34 +0100

    Game package
    Includes a game class that contains the players, the board and an array
    with all previous boards. Not sure yet if this is the correct place to
    represent all of this.

---
+++ src/game/Game.java
@@ -0,0 +1,42 @@
+package game;
+
+public class Game {
+	
+	//save the game number, might need it later for a leader board
+	private int gameNumber;
+	
+	private String player1 = null; //name of player1
+	private String player2 = null; //name of player2
+	private boolean complete = false; //checks if two players were added.
+	
+	/** The board, represented as a string */
+	private String board; //not sure whether this should go here. It should be specific to a client pair.
+	
+	/** All previous boards, to check whether a move is valid */
+	private String[] prevBoards; //not sure whether this should go here. It should be specific to a client pair.
+	
+	public Game(int number) {
+		gameNumber = number;
+	}
+	
+	public void addPlayer(String name) {
+		if(player1 == null) {
+			player1 = name;
+		} else {
+			player2 = name;
+			complete = true;
+		}
+	}
+	
+	/** returns whether two players were added to this game */
+	public boolean getCompleteness() {
+		return complete;
+	}
+	
+	/** return identified (=number) of the game */
+	public int getNumber() {
+		return gameNumber;
+	}
+	
+	
+}

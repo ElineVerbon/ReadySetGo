abfbe227baf6d36ab229259272b3cb6185319812
Eline Verbon <eline.verbon@nedap.com>
2020-01-21 09:42:31 +0100

    Make separate classes for checking the result of a move. Can be used by both Game and Players.

---
+++ src/movechecker/MoveResult.java
@@ -0,0 +1,11 @@
+package movechecker;
+
+/**
+ * MoveResult will check the result of a move.
+ * In other words, it will check whether stones need to be removed because of a move
+ * @author eline.verbon
+ *
+ */
+public class MoveResult {
+
+}
---
+++ src/movechecker/MoveValidator.java
@@ -0,0 +1,13 @@
+package movechecker;
+
+/**
+ * A class that checks whether a move is valid.
+ * 
+ * Can be used by both the players and the game.
+ * @author eline.verbon
+ *
+ */
+
+public class MoveValidator {
+
+}

47aecfdafb46512f5ae06fc14c1a3462d2891590
Eline Verbon <eline.verbon@nedap.com>
2020-01-21 18:08:10 +0100

    Run game tests using player input from a file.

--- src/tests/GameTest.java
+++ src/tests/GameTest.java
@@ -19,14 +19,15 @@ import game.Game;
 import protocol.ProtocolMessages;
 
 /**
- * Possible things to test (maybe divide over tests).
+ * This class will test whether the Game responds correctly to certain responses of players.
+ * Possible things to test (maybe divide over tests), responses to:
  * 
  * StartGameTest
  * - starting a game
  * 
  * MoveCheckerTest
- * - giving an invalid move
- * - checking whether stones are removed
+ * - invalid move
+ * - removal of stones
  * 
  * EndGameTest
  * - getting the score
@@ -44,50 +45,76 @@ import protocol.ProtocolMessages;
 public class GameTest {
 	private final static ByteArrayOutputStream OUTCONTENT = new ByteArrayOutputStream();
 	private final static PrintStream ORIGINALOUT = System.out;
-
-	Game testGame = new Game(1);
-	
-	private final static int BOARDDIMENSION = 5;
-	private String board;
-	
-	//Create two clients
-	GoClientHuman client1 = new GoClientHuman();
-	GoClientHuman client2 = new GoClientHuman();
 	
 	@BeforeAll
 	static public void setUpStream() {
 	    System.setOut(new PrintStream(OUTCONTENT));
 	}
 	
+	/**
+	 * Test game start.
+	 * 
+	 * Give one move for player 1. Except messages starting with 
+	 * G, T and R.
+	 * 
+	 * @throws FileNotFoundException 
+	 */
 	@Test
-	void testServer() 
-			throws ExitProgram, ServerUnavailableException, ProtocolException, IOException {
+	void testStartGame() throws FileNotFoundException  {
+		Game testGame = new Game(1);
 		
-		// Create a string representation of the empty board.
-        char[] charArray = new char[BOARDDIMENSION * BOARDDIMENSION];
-        Arrays.fill(charArray, ProtocolMessages.UNOCCUPIED);
-        board = new String(charArray);
-        
-        //
-        char colorPlayer1 = ProtocolMessages.BLACK;
-        char colorPlayer2 = ProtocolMessages.WHITE;
-		
-        //Make a String of commands for player 1 and set as inputStream for player1
-		//String startCommando1 = ProtocolMessages.GAME + ProtocolMessages.DELIMITER
-		//		+ board + ProtocolMessages.DELIMITER + colorPlayer1;
-		//but how do I know the new board?
-		//String turnCommando1 = ProtocolMessages.TURN + ProtocolMessages.DELIMITER + board;
-		String message1 = ProtocolMessages.MOVE + ProtocolMessages.DELIMITER + 3 + "\n";
-		BufferedReader inPlayer1 = new BufferedReader(new StringReader(message1));
+        //Use a file with a command per line to represent the moves of player1 as the bufferedReader
+		BufferedReader inPlayer1 = new BufferedReader(
+				new FileReader("/Users/eline.verbon/workspaces/proeftuin/FinalAssignment1"
+						+ "/src/tests/resources/startGameTest_MovesPlayer1.txt"));
 		StringWriter stringWriter1 = new StringWriter();
 		BufferedWriter outPlayer1 = new BufferedWriter(stringWriter1);
 		
-		//Make a String of commands for player 2 and set as inputStream for player2
-		String startCommando2 = ProtocolMessages.GAME + ProtocolMessages.DELIMITER
-				+ board + ProtocolMessages.DELIMITER + colorPlayer2;
-		String message2 = startCommando2;
-		BufferedReader inPlayer2 = new BufferedReader(new StringReader(message2));
-		BufferedWriter outPlayer2 = new BufferedWriter(new StringWriter());
+		//Use a file with a command per line to represent the moves of player2 as the bufferedReader
+		BufferedReader inPlayer2 = new BufferedReader(
+				new FileReader("/Users/eline.verbon/workspaces/proeftuin/FinalAssignment1"
+						+ "/src/tests/resources/startGameTest_MovesPlayer2.txt"));
+		StringWriter stringWriter2 = new StringWriter();
+		BufferedWriter outPlayer2 = new BufferedWriter(stringWriter2);
+		
+		//add players to game
+		testGame.addPlayer("Player1", inPlayer1, outPlayer1, "black");
+		testGame.addPlayer("Player2", inPlayer2, outPlayer2, "white");
+		
+		/**
+		 * Test start game.
+		 * This will 
+		 */
+		testGame.startGame();
+		assertThat(stringWriter1.toString(), containsString("G;UUUUUUUUUUUUUUUUUUUUUUUUU;B"));
+		assertThat(stringWriter1.toString(), containsString("T;UUUUUUUUUUUUUUUUUUUUUUUUU"));
+		assertThat(stringWriter1.toString(), containsString("R;V;UUUBUUUUUUUUUUUUUUUUUUUUU"));
+		
+		OUTCONTENT.reset();
+	}
+	
+	/**
+	 * 
+	 * @throws FileNotFoundException 
+	 */
+	
+	@Test
+	void testRemoveStones() throws FileNotFoundException {
+		Game testGame = new Game(2);
+		
+        //Use a file with a command per line to represent the moves of player1 as the bufferedReader
+		BufferedReader inPlayer1 = new BufferedReader(
+				new FileReader("/Users/eline.verbon/workspaces/proeftuin/FinalAssignment1"
+						+ "/src/tests/resources/removeStonesTest_MovesPlayer1.txt"));
+		StringWriter stringWriter1 = new StringWriter();
+		BufferedWriter outPlayer1 = new BufferedWriter(stringWriter1);
+		
+		//Use a file with a command per line to represent the moves of player2 as the bufferedReader
+		BufferedReader inPlayer2 = new BufferedReader(
+				new FileReader("/Users/eline.verbon/workspaces/proeftuin/FinalAssignment1"
+						+ "/src/tests/resources/removeStonesTest_MovesPlayer2.txt"));
+		StringWriter stringWriter2 = new StringWriter();
+		BufferedWriter outPlayer2 = new BufferedWriter(stringWriter2);
 		
 		//add players to game
 		testGame.addPlayer("Player1", inPlayer1, outPlayer1, "black");
@@ -102,12 +129,14 @@ public class GameTest {
 //			msg = inPlayer1.readLine();
 //		}
 		
-		//TODO I still need to test whether the startGame sends the correct message
-		testGame.startGame(); //dit werkt volgens mij niet meer
-		assertThat(stringWriter1.toString(), containsString("G;UUUUUUUUUUUUUUUUUUUUUUUUU;B"));
-		assertThat(stringWriter1.toString(), containsString("T;UUUUUUUUUUUUUUUUUUUUUUUUU"));
-		assertThat(stringWriter1.toString(), containsString("R;V;UUUBUUUUUUUUUUUUUUUUUUUUU"));
-		
+		/**
+		 * Test start game.
+		 * This will 
+		 */
+		testGame.startGame();
+		testGame.doTurn();
+		assertThat(stringWriter1.toString(), not(containsString("R;V;UBBUUBWWBUUBBWUUUWBWUUUWU")));
+		assertThat(stringWriter1.toString(), containsString("R;V;UBBUUBUUBUUBBWUUUWUWUUUWU"));
 		//Client prints a message to indicate the game has started
 		
 		//Test invalid move
